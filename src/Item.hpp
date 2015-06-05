90019ce9bd55c291437e178574158d9ac28d42c6
Clement Champetier <cnt@mikrosimage.eu>
2015-06-05 14:27:00 +0200

    Item: add constructor from a given sequence
    This function can be called from python binding (which is not the case
    of the same constructor with a boost::filesystem::path).

--- src/Item.hpp
+++ src/Item.hpp
@@ -60,6 +60,14 @@ public:
 		_path /= sequence.getStandardPattern();
 	}
 
+	Item( const Sequence& sequence, const std::string& folder )
+	: _type(eTypeSequence)
+	, _path(folder)
+	, _sequence(sequence)
+	{
+		_path /= sequence.getStandardPattern();
+	}
+
 	EType getType() const { return _type; }
 
 	std::string getAbsoluteFilepath() const { return _path.string(); }
